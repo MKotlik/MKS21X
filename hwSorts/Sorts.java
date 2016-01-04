13bc6974e9d51d614fc29835407f014c18c182b3
Mikhail Kotlik <mkotlik@stuy.edu>
2016-01-04 13:19:59 -0500

    Started sorts assignment

---
+++ hwSorts/Sorts.java
@@ -0,0 +1,16 @@
+public class Sorts{
+    public static void main(String[]args){
+	
+    }
+
+    public static void insertion(int[] array){
+	
+    }
+
+    public static void selection(int[] array){
+	int min = arr[0];
+	int swapIndex = 0;
+	int minIndex = 0;
+	int swapValue = 0;
+    }
+}
